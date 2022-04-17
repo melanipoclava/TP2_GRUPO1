85128cf7fb778cfe9371959308c9c680d3c8cf5e
HUMANA  GUSTAVO <HUMANA  GUSTAVO@LAPTOP-2M34BLRB>
2022-04-17 11:59:18 -0300

    1.creacion del paquete punto 17 2.creacion de la clase Principal y ProcesosFecha 3.definicion de motodos accesores y constructores solicitados 4.definicion de metodos solicitados 5.ejemplificacion de creacion de objetos y uso de metodos

---
+++ src/main/java/punto17/ProcesosFecha.java
@@ -0,0 +1,209 @@
+package punto17;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class ProcesosFecha {
+	//definicion de los atributos de la clase proceso fecha
+	LocalDate fecha1; 
+	LocalDate fecha2;
+	LocalTime time1;
+	LocalTime time2;
+	LocalDateTime dateTime1; 
+	LocalDateTime dateTime2;
+	
+	//constructor por defecto
+	public ProcesosFecha() {
+		// TODO Auto-generated constructor stub
+	}
+	//constructores parametrizado
+	/**
+	 * @param fecha1
+	 * @param fecha2
+	 */
+	public ProcesosFecha(LocalDate fecha1, LocalDate fecha2) {
+		super();
+		this.fecha1 = fecha1;
+		this.fecha2 = fecha2;
+	}
+	
+	/**
+	 * @param time1
+	 * @param time2
+	 */
+	public ProcesosFecha(LocalTime time1, LocalTime time2) {
+		super();
+		this.time1 = time1;
+		this.time2 = time2;
+	}
+	
+	
+	/**
+	 * @param dateTime1
+	 * @param dateTime2
+	 */
+	public ProcesosFecha(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+		super();
+		this.dateTime1 = dateTime1;
+		this.dateTime2 = dateTime2;
+	}
+	/**
+	 * @param fecha1
+	 * @param fecha2
+	 * @param time1
+	 * @param time2
+	 * @param dateTime1
+	 * @param dateTime2
+	 */
+	public ProcesosFecha(LocalDate fecha1, LocalDate fecha2, LocalTime time1, LocalTime time2, LocalDateTime dateTime1,
+			LocalDateTime dateTime2) {
+		super();
+		this.fecha1 = fecha1;
+		this.fecha2 = fecha2;
+		this.time1 = time1;
+		this.time2 = time2;
+		this.dateTime1 = dateTime1;
+		this.dateTime2 = dateTime2;
+	}
+
+
+
+	//Metodos Accesores
+	/**
+	 * @return the fecha1
+	 */
+	public LocalDate getFecha1() {
+		return fecha1;
+	}
+
+	/**
+	 * @param fecha1 the fecha1 to set
+	 */
+	public void setFecha1(LocalDate fecha1) {
+		this.fecha1 = fecha1;
+	}
+
+	/**
+	 * @return the fecha2
+	 */
+	public LocalDate getFecha2() {
+		return fecha2;
+	}
+
+	/**
+	 * @param fecha2 the fecha2 to set
+	 */
+	public void setFecha2(LocalDate fecha2) {
+		this.fecha2 = fecha2;
+	}
+
+	/**
+	 * @return the time1
+	 */
+	public LocalTime getTime1() {
+		return time1;
+	}
+
+	/**
+	 * @param time1 the time1 to set
+	 */
+	public void setTime1(LocalTime time1) {
+		this.time1 = time1;
+	}
+
+	/**
+	 * @return the time2
+	 */
+	public LocalTime getTime2() {
+		return time2;
+	}
+
+	/**
+	 * @param time2 the time2 to set
+	 */
+	public void setTime2(LocalTime time2) {
+		this.time2 = time2;
+	}
+
+	/**
+	 * @return the dateTime1
+	 */
+	public LocalDateTime getDateTime1() {
+		return dateTime1;
+	}
+
+	/**
+	 * @param dateTime1 the dateTime1 to set
+	 */
+	public void setDateTime1(LocalDateTime dateTime1) {
+		this.dateTime1 = dateTime1;
+	}
+
+	/**
+	 * @return the dateTime2
+	 */
+	public LocalDateTime getDateTime2() {
+		return dateTime2;
+	}
+
+	/**
+	 * @param dateTime2 the dateTime2 to set
+	 */
+	public void setDateTime2(LocalDateTime dateTime2) {
+		this.dateTime2 = dateTime2;
+	}
+	
+	// metodos solicitados
+	
+	public Period contarTiempoTranscurrido() {
+		Period periodo = Period.between(fecha1, fecha2);
+		return periodo;
+	}
+	
+	public String formatearFecha(LocalDate fecha) {
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String retorno=formato.format(fecha);
+		return (retorno);
+	}
+	
+	public String compararFechas() {
+		String rta;
+		//compara si fecha1 es despues que fecha2
+		if (fecha1.isAfter(fecha2))
+			rta= "la fecha "+fecha1+" es porterior a la fecha "+fecha2;
+		else
+			//compara si fecha1 es antes que fecha2
+			if (fecha1.isBefore(fecha2))
+				rta= "la fecha "+fecha1+" es anterior a la fecha "+fecha2;
+			else
+				rta= "la fecha "+fecha1+" y la fecha"+fecha2+"son iguales";
+			
+		return (rta);
+		
+	}
+	
+	//
+	public void decremento() {
+		time1= time1.plusHours(5);
+		time2=time2.minusMinutes(20);
+		System.out.println(" incrementado "+ time1 +"decremetado"+time2);
+			}
+	//procesp incrementa deias usa las variables trasncurso para tomar el valor que se desea incrementar
+	//y typo para diferenciar la opcion dia mes y año
+	public LocalDate incrementar(LocalDate fech,int transcurso, String typo) {
+		LocalDate fechaincr=fech;
+		if (typo=="M") {
+			fechaincr= fech.plusMonths(transcurso);
+		}else if(typo=="D") {
+			fechaincr= fech.plusDays(transcurso);
+		}else if (typo=="Y") {
+			fechaincr= fech.plusYears(transcurso);
+		}
+		return fechaincr;
+	}
+}
+
+
---
+++ src/main/java/punto17/principal.java
@@ -0,0 +1,59 @@
+package punto17;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ProcesosFecha pf1 = new ProcesosFecha(LocalDate.of(2022,01,21),LocalDate.of(2021,11,21));
+		System.out.println(pf1.getFecha1()+" "+pf1.getFecha2());
+		//b2 
+		
+		ProcesosFecha pf2 = new ProcesosFecha(LocalTime.parse("10:11:31"),LocalTime.parse("20:12:01"));
+		System.out.println("timpo registrado: "+pf2.getTime1() +"tiempo registrado: "+pf2.getTime2());
+		pf2.decremento();
+		
+		//b3
+		
+		System.out.println(pf1.compararFechas());
+		
+		//b4
+		
+		ProcesosFecha pf3= new ProcesosFecha(LocalDateTime.of(pf1.fecha1, pf2.time1),LocalDateTime.of(pf1.fecha2, pf2.time2));
+		System.out.println(pf3.dateTime1+" "+pf3.dateTime2);
+		
+		//b5
+		
+		ProcesosFecha pf4= new ProcesosFecha(LocalDate.of(1998, 11, 11),LocalDate.now());
+		System.out.println("Han Transcurrido "+pf4.contarTiempoTranscurrido().getYears()+" años, "+pf4.contarTiempoTranscurrido().getMonths()+" meses, "+pf4.contarTiempoTranscurrido().getDays()+" dias");
+	   //b6
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("ingrese fecha en este  formato dd/MM/yyyy");
+		String fcha = scan.nextLine();
+		DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate dfech = LocalDate.parse(fcha, formateador);
+		ProcesosFecha pf5 = new ProcesosFecha();
+		pf5.setFecha1(dfech);
+		LocalDate dfech2 = pf5.incrementar(pf5.getFecha1(),365,"D");
+		System.out.println(pf5.getFecha1()+" ==>"+dfech2);
+		 
+		System.out.println("ingrese fecha en este  formato dd/MM/yyyy");
+		String fcha1 = scan.nextLine();
+		LocalDate ffech = LocalDate.parse(fcha1, formateador);
+		pf5.setFecha2(ffech);
+		LocalDate ffech2 = pf5.incrementar(pf5.getFecha2(),6,"M");
+		System.out.println(pf5.getFecha2()+" ==>"+ffech2);
+	}
+	
+	
+
+}
