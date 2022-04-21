4875ad3227c6e155ebef5b061529532d55e8c889
melan <melan@DESKTOP-JIO3JRI>
2022-04-21 17:12:34 -0300

    punto 7 actualizado

--- src/main/java/punto7/Ejercicio7.java
+++ src/main/java/punto7/Ejercicio7.java
@@ -3,26 +3,27 @@ public class Ejercicio7 {
 
 	public static void main(String[] args) 
 	{
-	int j = 40;
-	for(int i = 2; i <= 80; i=i+2) {
-	if(i == 2) {
-			j = (j+j);
-			System.out.println(j);
-			i++;
-		}
-				if(i <= 40) {
-					j = (40 + j - i);
-					System.out.println(j);
-							}
-				else {
-					j =(j+ 40 - i);
-					System.out.println(j);
-					  }		
-					
+		 int i;
+		int j;
+				for(i = 2, j = 40; i <= 80; i+=2) {
+					if(i == 2) {
+						j += j; 
+						System.out.println(j + " ");
+						i++;
+					}
+					if(i <= 40) {
+						j = 40 + j - i;
+						System.out.println(j + " ");
+					}else {
+						j += 40 - i;
+						System.out.println(j + " ");
+					}	
+				}
 			}
-	}
-
 }
+	
+
+
 
 
 
