dcab05127efb40fe4d32e7242ea2b89f9b23351c
melan <melan@DESKTOP-JIO3JRI>
2022-04-24 00:17:50 -0300

    punto 7 actualización

--- src/main/java/punto7/Ejercicio7.java
+++ src/main/java/punto7/Ejercicio7.java
@@ -2,13 +2,13 @@ package punto7;
 public class Ejercicio7 {
 
 	public static void main(String[] args) 
-	{
+	{   
 		 int i;
 		int j;
 				for(i = 2, j = 40; i <= 80; i+=2) {
 					if(i == 2) {
 						j += j; 
-						System.out.println(j + " ");
+						System.out.println(j + "   ");
 						i++;
 					}
 					if(i <= 40) {
